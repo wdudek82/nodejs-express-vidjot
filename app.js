a74d58b45b9f5c34c2e8aa2d23893c51053b9363
Wojciech Dudek <wdudek82@gmail.com>
2018-04-20 22:51:19 +0200

    Added Idea model, and route/template for 'add idea'

--- app.js
+++ app.js
@@ -9,17 +9,15 @@ mongoose.connect('mongodb://localhost/vidjot-dev')
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-
-const port = 5000;
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
 
 // Index route
 app.get('/', (req, res) => {
@@ -39,4 +37,21 @@ app.get('/about', (req, res) => {
 // Projects route
 app.get('/projects', (req, res) => {
   res.render('projects');
+});
+
+// Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Ideas List
+app.get('/ideas', (req, res) => {
+  res.render('ideas');
+});
+
+// Server
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
